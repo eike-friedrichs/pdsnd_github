642e7eb05b80343c713fd2f93a46aeffc909afe7
Eike Friedrichs <eike.friedrichs@bayer.com>
2021-12-02 20:35:36 +0100

    docs: improved code docu

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,7 +1,9 @@
+# import section
 import time
 import pandas as pd
 import numpy as np
 
+# constant helpers
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
